6a51cca8b27122bbfb9c0f1a345d5858eed38819
RidaOuledhaddou <reda7ouledhaddou@gmail.com>
2024-07-04 11:49:46 +0000

    0x00-python_variable_annotations

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """
-This module contains a function to join two strings together.
+This is a module that provides a function for concatenating two strings.
 """
+
 
 def concat(str1: str, str2: str) -> str:
     """
-    Concatenates two strings and returns the combined result.
+    This function concatenates two strings and returns the result.
 
     Parameters:
-    str1 (str): The first input string.
-    str2 (str): The second input string.
+    str1 (str): The first string.
+    str2 (str): The second string.
 
     Returns:
-    str: The combined string resulting from concatenating str1 and str2.
+    str: The concatenation of str1 and str2.
     """
     return str1 + str2
-
